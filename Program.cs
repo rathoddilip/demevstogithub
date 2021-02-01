d0926f5636717e3b7219fc50ce723ca69df7fbe6
diliprathod32 <diliprathod32@gmail.com>
2021-02-01 23:14:43 +0530

    [dilip]:added controlstatementprogram

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace controlStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whether number is greater or less than 10");
+            int number1, number2;
+            Console.WriteLine("Enter a number1");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number2");
+            number2 = Convert.ToInt32(Console.ReadLine());
+           
+            if (number1 + number2 > 10)
+            {
+                Console.WriteLine("The number is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The number is not greater than 10");
+            }
+
+        }
+    }
+}
